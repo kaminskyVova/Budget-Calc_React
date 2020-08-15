1201a411c7209fd67ab95b6fb55a702e18b02ae3
unknown <kamok@bk.ru>
2020-08-15 19:42:16 +0300

    first

--- src/App.js
+++ src/App.js
@@ -1,26 +1,67 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React, { Component } from "react";
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+import Total from "./components/Total";
+import Operation from "./components/Operation";
+import History from "./components/history/History";
+import Header from "./components/Header";
+
+class App extends Component {
+  state = {
+    transactions: [],
+    description: "",
+    amount: "",
+  };
+
+  addTransaction = (add) => {
+    const transactions = [
+      ...this.state.transactions,
+      {
+        id: `cmr${(+new Date()).toString(16)}`,
+        description: this.state.description,
+        amount: this.state.amount,
+        add,
+      },
+    ];
+
+    this.setState({
+      transactions,
+      description: "",
+      amount: ""
+    });
+  };
+
+  addAmount = (e) => {
+    this.setState({
+      amount: e.target.value,
+    });
+  };
+
+  addDescription = (e) => {
+    this.setState({
+      description: e.target.value,
+    });
+  };
+
+  render() {
+    return (
+      <div className="App">
+        <main>
+          <div className="container">
+            <Header />
+            <Total />
+            <History transactions={this.state.transactions}/>
+            <Operation
+              addTransaction={this.addTransaction}
+              addAmount={this.addAmount}
+              addDescription={this.addDescription}
+              description={this.state.description}
+              amount={this.state.amount}
+            />
+          </div>
+        </main>
+      </div>
+    );
+  }
 }
 
 export default App;
---
+++ src/components/Header.jsx
@@ -0,0 +1,16 @@
+import React, { Component } from "react";
+
+class Header extends Component {
+  render() {
+    return (
+      <div>
+        <header>
+          <h1>Кошелек</h1>
+          <h2>Калькулятор расходов</h2>
+        </header>
+      </div>
+    );
+  }
+}
+
+export default Header;
